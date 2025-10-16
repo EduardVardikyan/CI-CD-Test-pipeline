bef7fbdea8f99aa372488091ff8d306a89e8ceea
EduardVardikyan <97230666+EduardVardikyan@users.noreply.github.com>
2025-10-16 15:43:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! From Docker + GitHub Actions"
+    return "Hello, World! From Docker + GitHub Actions La-La-La"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
